273395e1e559d5321f77c1c2cabaa985c66c6403
comstudyschool <comstudychool@gmail.com>
2024-08-13 13:26:24 +0900

    react day02 컴포넌트 생성

--- day02/day02react01/src/index.js
+++ day02/day02react01/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const App = () => {
-  return(<>
-    <h3>Welcome to my homepage</h3>
-    <h1>Hello World</h1>
-  </>);
-};
+import App from './App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
